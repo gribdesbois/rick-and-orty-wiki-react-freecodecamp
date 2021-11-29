ca80047b5f93188693b369b52aa74a7133df5ff9
gribdesbois <gribdesbois@gmail.com>
2021-11-29 13:40:21 +0100

    pb solved

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -10,52 +10,46 @@ function Card({ page, results }) {
       const {
         id, image, name, status, location,
       } = x
-
       return (
         <Link
           style={{ textDecoration: 'none' }}
           to={`${page}${id}`}
           key={id}
-          className='col-lg-4 col-md-6 col-12 mb-4 position-relative text-dark'
+          className='col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark'
         >
           <div
-            key={id}
-            className='col-lg-4 col-md-6 col-sm-6 col-12 mb-4 position-relative text-dark'
+            className={`${styles.card} d-flex flex-column justify-content-center`}
           >
-            <div
-              className={`${styles.card} d-flex flex-column justify-content-center`}
-            >
-              <img className={`${styles.img} img-fluid`} src={image} alt="" />
-              <div className={`${styles.content}`} >
-                <div className='fs-5 fw-bold mb-4'>{name}</div>
-                <div className=''>
-                  <div className='fs-6 fw-normal'>Last Location</div>
-                  <div className='fs-5'>{location.name}</div>
-                </div>
+            <img className={`${styles.img} img-fluid`} src={image} alt="" />
+            <div className={`${styles.content}`} >
+              <div className='fs-5 fw-bold mb-4'>{name}</div>
+              <div className=''>
+                <div className='fs-6 fw-normal'>Last Location</div>
+                <div className='fs-5'>{location.name}</div>
               </div>
-              {
-                (() => {
-                  if (status === 'Dead') {
-                    return (
-                      <div className={`${styles.badge} position-absolute badge bg-danger`}>
-                        {status}
-                      </div>
-                    )
-                  } if (status == 'Alive') {
-                    return (
-                      <div className={`${styles.badge} position-absolute badge bg-success`} >
-                        {status}
-                      </div>
-                    )
-                  }
+            </div>
+            {
+              (() => {
+                if (status === 'Dead') {
                   return (
-                    <div className={`${styles.badge} position-absolute badge bg-secondary`} >
+                    <div className={`${styles.badge} position-absolute badge bg-danger`}>
                       {status}
                     </div>
                   )
-                })()
-              }
-            </div>
+                } if (status == 'Alive') {
+                  return (
+                    <div className={`${styles.badge} position-absolute badge bg-success`} >
+                      {status}
+                    </div>
+                  )
+                }
+                return (
+                  <div className={`${styles.badge} position-absolute badge bg-secondary`} >
+                    {status}
+                  </div>
+                )
+              })()
+            }
           </div>
         </Link>
       )
